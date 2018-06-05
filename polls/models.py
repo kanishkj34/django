08f159c9a2a4436e8847896c742dd4e03a26fc9a
kanishkj34 <kanishkj34@gmail.com>
2018-06-05 15:04:45 +0530

    First commit

---
+++ polls/models.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+#from django.db import models
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+#@python_2_unicode_compatible  # only if you need to support Python 2
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+
+#@python_2_unicode_compatible  # only if you need to support Python 2
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
